7c815f78e979dbd28c8ff44bc072e795388348e3
Luke <luke@oddthread.com>
2022-11-02 03:48:08 -0500

    headers

--- src/c/oul.c
+++ src/c/oul.c
@@ -11,8 +11,8 @@
     #include "SDL_ttf.h"
     #else
 	#include <SDL2/SDL.h>
-	#include <SDL2/SDL_ttf.h>
-	#include <SDL2/SDL_image.h>
+	#include <SDL2_ttf/SDL_ttf.h>
+	#include <SDL2_image/SDL_image.h>
     #endif
 #else
 #include <SDL2/SDL.h>
